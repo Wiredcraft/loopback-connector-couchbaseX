8653f1415485af38b03549d5310d64ede14b6d48
CCharlieLi <ccharlieli@live.com>
2015-09-18 02:26:12 +0800

    add and update CRUD methods and tests

--- test/CRUD.test.js
+++ test/CRUD.test.js
@@ -1,5 +1,6 @@
 // This test written in mocha
 var should = require('./init.js');
+var assert = require('assert');
 var db = getDataSource();
 
 describe('Couchbase CRUD methods', function() {
@@ -7,29 +8,62 @@ describe('Couchbase CRUD methods', function() {
 
   it('create', function(done) {
     Person.create({
-      id: 'asd',
+      id: '1',
       name: 'Charlie',
-      age: 370
+      age: 24
     }, function(err, person) {
-      person.age.should.equal(370);
-      done(err, person);
-    });
-  });
-
-  it('exists', function(done) {
-    Person.exists('asd', function(err, person) {
-      console.log(err);
-      customer.name.should.equal('Charlie');
+      person.age.should.equal(24);
       done(err, person);
     });
   });
 
   it('find', function(done) {
-    Person.find({id: 'asd'}, function(err, person) {
-      console.log(err);
-      customer.name.should.equal('Charlie');
+    Person.find({id: '1'}, function(err, person) {
+      person.name.should.equal('Charlie');
       done(err, person);
     });
   });
+
+  it('update', function(done) {
+    Person.update({
+      id: '1'
+    },{
+      id: '1',
+      name: 'Mary',
+      age: 37
+    }, function(err, res) {
+      assert.equal(res, true);
+      done(err, res);
+    });
+  });
+
+  it('destroy', function(done) {
+    Person.remove({
+      id: '1'
+    },function(err, res) {
+      assert.equal(res, true);
+      done(err, res);
+    });
+  });
+
+  // it('count', function(done) {
+  //   Person.count({id: 'asd'}, function(err, person) {
+  //     console.log(err);
+  //     console.log(JSON.stringify(person));//person.name.should.equal('Charlie');
+  //     done(err, person);
+  //   });
+  // });
+
+  // it('exists', function(done) {
+  //   Person.exists('asd', function(err, person) {
+  //     console.log(err);
+  //     person.name.should.equal('Charlie');
+  //     done(err, person);
+  //   });
+  // });
+
+  
+
+
 
 });
