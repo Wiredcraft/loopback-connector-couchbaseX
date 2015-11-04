34f37bade82602a76eee832bef6214d53f20ee6e
CCharlieLi <ccharlieli@live.com>
2015-11-04 17:47:17 +0800

    Add tests for save() and destroy()

--- test/CRUD.test.js
+++ test/CRUD.test.js
@@ -21,6 +21,11 @@ describe('Couchbase CRUD methods', function() {
         age: 24
       },
       {
+        id: '3',
+        name: 'David',
+        age: 24
+      },
+      {
         name: 'Jason',
         age: 44
       }
@@ -42,7 +47,7 @@ describe('Couchbase CRUD methods', function() {
   });
 
   it('create should create new document without giving id', function(done) {
-    return Person.create(persons[2]).then(function(person) {
+    return Person.create(persons[3]).then(function(person) {
       person.name.should.equal('Jason');
       done();
     }).catch(function(err) {
@@ -52,6 +57,48 @@ describe('Couchbase CRUD methods', function() {
 
   it('create error with exist id ', function(done) {
     return Person.create(persons[0]).then().catch(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  //SAVE TEST
+  it('save/create one instace by its prototype method', function(done) {
+    var person = Person(persons[2]);
+    return person.save().then(function(person) {
+      person.name.should.eql('David');
+      done();
+    }).catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('save/create one instace without giving id', function(done) {
+    var person = Person(persons[3]);
+    return person.save().then(function(person) {
+      person.name.should.equal('Jason');
+      done();
+    }).catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('save a exist instace after changed', function(done) {
+    return Person.findOne({where: {id: persons[2].id}}).then(function(person) {
+      person.value.name.should.eql('David');
+      person.value.name = 'Charlie';
+      person.save().then(function(person) {
+        person.value.name.should.eql('Charlie');
+        done();
+      });
+    }).catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('save/create error with exist id', function(done) {
+    var person = Person(persons[2]);
+    return person.save().then().catch(function(err) {
       should.exist(err);
       done();
     });
@@ -155,26 +202,48 @@ describe('Couchbase CRUD methods', function() {
   });
 
   //DESTROY TEST
-  it('remove one exist document by id', function(done) {
+  it('remove one exist document with removeById()', function(done) {
     return Person.removeById(persons[0].id).then(function() {
-      Person.removeById(persons[1].id).then(function() {
-        done();
-      });
+      done();
     }).catch(function(err) {
       done(err);
     });
   });
 
-  // it('remove one exist document(where)', function(done) {
-  //  return Person.remove({name: 'Jason'}).then(function() {
-  //    done();
-  //  }).catch(function(err) {
-  //    done(err);
-  //  });
-  // });
+  it('remove one exist document with remove()', function(done) {
+    return Person.remove({id: persons[1].id}).then(function() {
+      done();
+    }).catch(function(err) {
+      done(err);
+    });
+  });
 
-  it('destroy error when unmatched documnet', function(done) {
+  it('remove one instace by its prototype method', function(done) {
+    var person = Person(persons[2]);
+    return person.remove().then(function(person) {
+      done();
+    }).catch(function(err) {
+      done(err);
+    });
+  });
+
+  it('remove error by its prototype method when instace doesn\'t exist', function(done) {
+    var person = Person(persons[2]);
+    return person.remove().then().catch(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('remove error when use remove() with unmatched documnet', function(done) {
     return Person.remove({id: uuid.v4()}).then().catch(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+
+  it('remove error when use removeById() with unmatched documnet', function(done) {
+    return Person.removeById(uuid.v4()).then().catch(function(err) {
       should.exist(err);
       done();
     });
