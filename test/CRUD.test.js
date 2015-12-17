0e99aea1e3ee63b23c59961105077ab0fe343f16
Makara Wang <makara15@gmail.com>
2015-12-17 16:31:07 +0800

    Reviewed all CRUD methods, but disabled bulk operations for now.

--- test/CRUD.test.js
+++ test/CRUD.test.js
@@ -21,15 +21,15 @@ describe('Couchbase CRUD', function() {
       age: Number
     });
     persons = [{
-      id: '1',
+      id: '0',
       name: 'Charlie',
       age: 24
     }, {
-      id: '2',
+      id: '1',
       name: 'Mary',
       age: 24
     }, {
-      id: '3',
+      id: '2',
       name: 'David',
       age: 24
     }, {
@@ -46,15 +46,15 @@ describe('Couchbase CRUD', function() {
     });
 
     it('can create an instance with an id', function(done) {
-      return Person.create(persons[0]).then(function(person) {
-        person.id.should.equal('1');
+      Person.create(persons[0]).then(function(person) {
+        person.id.should.equal('0');
         person.name.should.equal('Charlie');
         done();
       }).catch(done);
     });
 
     it('can create an instance without an id', function(done) {
-      return Person.create(persons[3]).then(function(person) {
+      Person.create(persons[3]).then(function(person) {
         person.id.should.be.String();
         person.name.should.equal('Jason');
         done();
@@ -62,7 +62,7 @@ describe('Couchbase CRUD', function() {
     });
 
     it('cannot create with a duplicate id ', function(done) {
-      return Person.create(persons[0]).then(function() {
+      Person.create(persons[0]).then(function() {
         done(new Error('expected an error'));
       }, function(err) {
         should.exist(err);
@@ -87,9 +87,9 @@ describe('Couchbase CRUD', function() {
     });
 
     it('can find a saved instance', function(done) {
-      Person.findById('1').then(function(person) {
+      Person.findById('0').then(function(person) {
         person.should.be.Object();
-        person.id.should.equal('1');
+        person.id.should.equal('0');
         person.name.should.equal('Charlie');
         person.age.should.equal(24);
         done();
@@ -97,7 +97,7 @@ describe('Couchbase CRUD', function() {
     });
 
     it('cannot find an unsaved instance', function(done) {
-      Person.findById('2').then(function() {
+      Person.findById('1').then(function() {
         done(new Error('expected an error'));
       }, function(err) {
         should.exist(err);
@@ -123,7 +123,7 @@ describe('Couchbase CRUD', function() {
 
     it('can destroy a saved instance', function(done) {
       var person = Person(persons[0]);
-      return person.remove().then(function(res) {
+      person.remove().then(function(res) {
         res.should.be.Object().with.property('count', 1);
         done();
       }).catch(done);
@@ -131,7 +131,39 @@ describe('Couchbase CRUD', function() {
 
     it('cannot destroy an unsaved instance', function(done) {
       var person = Person(persons[2]);
-      return person.remove().then(function() {
+      person.remove().then(function() {
+        done(new Error('expected an error'));
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    // TODO: more errors
+  });
+
+  describe('Destroy by ID', function() {
+    before(function(done) {
+      Person.create(persons[0]).then(function() {
+        done();
+      }, done);
+    });
+
+    after(function(done) {
+      connector.manager().call('flushAsync').then(function() {
+        done();
+      }, done);
+    });
+
+    it('can destroy a saved instance', function(done) {
+      Person.destroyById('0').then(function(res) {
+        res.should.be.Object().with.property('count', 1);
+        done();
+      }).catch(done);
+    });
+
+    it('cannot destroy an unsaved instance', function(done) {
+      Person.destroyById('2').then(function() {
         done(new Error('expected an error'));
       }, function(err) {
         should.exist(err);
@@ -157,12 +189,12 @@ describe('Couchbase CRUD', function() {
 
     it('can update an instance', function(done) {
       Person.updateOrCreate({
-        id: '1',
+        id: '0',
         name: 'Charlie II',
         age: 24
       }).then(function(res) {
         res.should.be.Object();
-        res.should.have.property('id', '1');
+        res.should.have.property('id', '0');
         res.should.have.property('name', 'Charlie II');
         res.should.have.property('age', 24);
         done();
@@ -172,7 +204,7 @@ describe('Couchbase CRUD', function() {
     it('can create an instance', function(done) {
       Person.updateOrCreate(persons[1]).then(function(res) {
         res.should.be.Object();
-        res.should.have.property('id', '2');
+        res.should.have.property('id', '1');
         res.should.have.property('name', 'Mary');
         res.should.have.property('age', 24);
         done();
@@ -196,11 +228,11 @@ describe('Couchbase CRUD', function() {
     });
 
     it('can update an instance', function(done) {
-      var person = Person.findById('1').then(function(person) {
+      var person = Person.findById('0').then(function(person) {
         person.name = 'Charlie II';
         person.save().then(function(res) {
           res.should.be.Object();
-          res.should.have.property('id', '1');
+          res.should.have.property('id', '0');
           res.should.have.property('name', 'Charlie II');
           res.should.have.property('age', 24);
           done();
@@ -212,7 +244,7 @@ describe('Couchbase CRUD', function() {
       var person = Person(persons[1]);
       person.save().then(function(res) {
         res.should.be.Object();
-        res.should.have.property('id', '2');
+        res.should.have.property('id', '1');
         res.should.have.property('name', 'Mary');
         res.should.have.property('age', 24);
         done();
@@ -222,10 +254,114 @@ describe('Couchbase CRUD', function() {
     // TODO: more errors
   });
 
-  describe('Find multiple', function() {});
+  describe.skip('Find multiple', function() {
+    before(function(done) {
+      Person.create(persons[0]).then(function() {
+        done();
+      }, done);
+    });
 
-  describe('Update multiple', function() {});
+    before(function(done) {
+      Person.create(persons[1]).then(function() {
+        done();
+      }, done);
+    });
 
-  describe('Destroy multiple', function() {});
+    after(function(done) {
+      connector.manager().call('flushAsync').then(function() {
+        done();
+      }, done);
+    });
+
+    it('can find 2 instances', function(done) {
+      Person.findByIds(['0', '1']).then(function(res) {
+        res.should.be.Array().with.length(2);
+        done();
+      }).catch(done);
+    });
+
+    it('cannot find wrong instances', function(done) {
+      Person.findByIds(['0', 'lorem']).then(function() {
+        done(new Error('expected an error'));
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('can find 2 instances', function(done) {
+      Person.find({
+        where: {
+          id: {
+            inq: ['0', '1']
+          }
+        }
+      }).then(function(res) {
+        res.should.be.Array().with.length(2);
+        done();
+      }).catch(done);
+    });
+
+    it('cannot find wrong instances', function(done) {
+      Person.find({
+        where: {
+          id: {
+            inq: ['0', 'lorem']
+          }
+        }
+      }).then(function() {
+        done(new Error('expected an error'));
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
+
+  describe.skip('Destroy multiple', function() {
+    before(function(done) {
+      Person.create(persons[0]).then(function() {
+        done();
+      }, done);
+    });
+
+    before(function(done) {
+      Person.create(persons[1]).then(function() {
+        done();
+      }, done);
+    });
+
+    after(function(done) {
+      connector.manager().call('flushAsync').then(function() {
+        done();
+      }, done);
+    });
+
+    it('can remove 2 instances', function(done) {
+      Person.remove({
+        id: {
+          inq: ['0', '1']
+        }
+      }).then(function(res) {
+        res.should.deepEqual({
+          count: 2
+        });
+        done();
+      }).catch(done);
+    });
+
+    it('cannot remove them again', function(done) {
+      Person.remove({
+        id: {
+          inq: ['0', '1']
+        }
+      }).then(function() {
+        done(new Error('expected an error'));
+      }, function(err) {
+        should.exist(err);
+        done();
+      });
+    });
+  });
 
 });
